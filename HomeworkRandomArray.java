476ceefaef588ebf87ee51b87ab7fffa0deca505
AlexgorAlexgor <126571657+AlexgorAlexgor@users.noreply.github.com>
2023-04-15 00:25:13 +0200

    Update HomeworkRandomArray.java

--- HomeworkRandomArray.java
+++ HomeworkRandomArray.java
@@ -3,8 +3,10 @@ import java.util.Random;
 //Create an array of 15 random integers from the range [0;9]. 
 //Count how many even elements are in the array and print this number to the screen on a separate line
 
-public class Main {
-  public static void main(String[] args) {
+public class HomeworkRandomArray
+ {
+  public static void main(String[] args)
+  {
     int[] arr = new int[15];
     Random rand = new Random();
 
@@ -15,8 +17,10 @@ public class Main {
 
     // count the number of even elements in the array
     int count = 0;
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 == 0) {
+    for (int i = 0; i < arr.length; i++) 
+    {
+      if (arr[i] % 2 == 0) 
+      {
         count++;
       }
     }
